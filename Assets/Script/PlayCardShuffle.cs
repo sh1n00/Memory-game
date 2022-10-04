2171fe616c796860e0bdac65aa441e307a8158c4
Hiroyuki Ishizuka <56533309+yuk4869@users.noreply.github.com>
2022-10-04 21:16:06 +0900

    feat:Shuffle

---
+++ Assets/Script/PlayCardShuffle.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayCardShuffle : MonoBehaviour
+{
+    //public List<GameObject> ObjectList = new List<GameObject>();
+    public Transform AllCard;
+    private int ObjectCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var cardList = GetChildren(AllCard);
+        Shuffle(cardList);
+        Placement(cardList);
+        for(var i = 0; i < cardList.Length; ++i)
+        {
+            Debug.Log(cardList[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static Transform[] GetChildren(Transform parent)
+    {
+        var children = new Transform[parent.childCount];
+
+        for(var i = 0; i < children.Length; ++i)
+        {
+            children[i] = parent.GetChild(i);
+        }
+        return children;
+    }
+    private void Shuffle(Transform[] cardList)
+    {
+        int n = cardList.Length;
+        while(n > 1)
+        {
+            n--;
+            int k = Random.Range(0, n + 1);
+
+            var temp = cardList[k];
+            cardList[k] = cardList[n];
+            cardList[n] = temp;
+
+        }
+    }
+    private void Placement(Transform[] cardList)
+    {
+        int n = cardList.Length;
+        float iniX = -0.4f;
+        float iniY = 0f;
+        float iniZ = 0.165f;
+        var x = iniX;
+        var y = iniY;
+        var z = iniZ;
+
+
+        while(n > 0)
+        {
+            n--;
+
+            if (x > 0.445f)
+            {
+                x = iniX;
+                z -= 0.11f;
+            }
+            cardList[n].position = new Vector3(x, y, z);
+            x += 0.065f;
+            
+        }
+    }
+}
