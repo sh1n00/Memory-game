9d200f6e38cbdb2e21f2814102fff676ff406137
Hikaru7777 <HikaruHigashiyama.NAIST.7@gmail.com>
2022-10-04 21:30:58 +0900

    ReverseCard

---
+++ Assets/Script/ReverseCard.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ReverseCard : MonoBehaviour, IPointerClickHandler
+{
+    //float angularSpeed = 180f;クリックするたびに回転するコード  
+    public void OnPointerClick(PointerEventData eventData)
+    {
+       // transform.eulerAngles += angularSpeed * Vector3.forward;クリックするたびに回転するコード
+        transform.rotation = Quaternion.AngleAxis(0, new Vector3(1, 0, 0));
+        Debug.Log("Click");
+    }
+}
